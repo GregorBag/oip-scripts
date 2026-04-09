6b5c922d5191de90df0b2e8aae090cb4fc230095
GregorBag <gregorbag@gmail.com>
2026-04-09 12:27:35 +0300

    auto-light

---
+++ get-led/auto-light.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+led = 26
+GPIO.setup(led, GPIO.OUT)
+
+foto_trans = 6
+GPIO.setup(led, GPIO.IN)
+
+state = 1
+
+while True:
+    if GPIO.input(foto_trans):
+        state = not state
+        GPIO.output(led, state)
+        time.sleep(0.2)
