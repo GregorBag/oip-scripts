1cc5479dfd342474c876bec7535d1b550e07fdc2
GregorBag <gregorbagd@gmail.com>
2026-04-16 12:07:11 +0300

    8-bit.v2

--- get-dac/8-bit-dac-manual.py
+++ get-dac/8-bit-dac-manual.py
@@ -7,7 +7,6 @@ dac_bits = [16, 20, 21, 25, 26, 17, 27, 22]
 GPIO.setup(dac_bits, GPIO.OUT)
 
 dynamic_range = 3.3
-volts = 0
 
 def voltage_to_number(voltage):
     if not (0.0 <= voltage <= dynamic_range):
@@ -18,7 +17,8 @@ def voltage_to_number(voltage):
     return int(voltage / dynamic_range * 255)
 
 def number_to_dac(value):
-    return [int(element) for element in bin(value)[2:].zfill(8)]
+    num = [int(element) for element in bin(value)[2:].zfill(8)]
+    GPIO.output(dac_bits, num)
 
 try:
     while True:
