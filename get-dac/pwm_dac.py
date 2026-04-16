05e17f4e149e254b65e4a1513a04dca3bce90044
GregorBag <gregorbagd@gmail.com>
2026-04-16 12:59:16 +0300

    pwm-dac

---
+++ get-dac/pwm_dac.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+
+class PWM_DAC:
+    def __init__(self, gpio_pin, pwm_frequency, dynamic_range, verbose = False):
+        self.gpio_pin = gpio_pin
+        self.dynamic_range = dynamic_range
+        self.pwm_frequency = pwm_frequency
+        self.verbose = verbose
+        
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.gpio_pin, GPIO.OUT, initial = 0)
+        self.pwm = GPIO.PWM(gpio_pin, pwm_frequency)
+
+    def deinit(self):
+        GPIO.output(self.gpio_pin, 0)
+        GPIO.cleanup()
+
+    def set_voltage(self, voltage):
+        if not (0.0 <= voltage <= self.dynamic_range):
+            print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {self.dynamic_range:.2f} В)")
+            print("Устанавлниваем 0.0 В")
+            return 0
+    
+        procent = voltage / self.dynamic_range * 100
+        if self.verbose:
+            print(f"Коэффициент заполнения: {k:.1f}%")
+        self.pwm.ChangeDutyCycle(procent)
+
+if __name__ == "__main__":
+    try:
+        dac = PWM_DAC(16, 500, 3.290, True)
+        
+        while True:
+            try:
+                voltage = float(input("Введите напряжение в Вольтах: "))
+                dac.set_voltage(voltage)
+
+            except ValueError:
+                print("Вы ввели не число. Попробуйте ещё раз\n")
+
+    finally:
+        dac.deinit()
