c7f54e0b0a20b897676d274d676069d55d7138a3
GregorBag <gregorbagd@gmail.com>
2026-04-23 11:46:39 +0300

    sin1

---
+++ get-dac/signal_generator.py
@@ -0,0 +1,12 @@
+from numpy import sin, pi, floor
+from time import sleep
+
+
+def get_sin_wave_amplitude(freq: float, time: float):
+    return (sin(2.0 * pi * freq * time) + 1.0) / 2.0
+
+def wait_for_sampling_period(sampling_frequency: float):
+    sleep(1.0 / sampling_frequency)
+
+# def get_triangle_amplitude(freq: float, time: float):
+#     return abs(2.0 * (time * freq - floor(time * freq)) - 1)
