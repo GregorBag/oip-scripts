cd0889023dbacbbe39fc6d0bfc73085e4a8bb127
GregorBag <gregorbagd@gmail.com>
2026-04-23 12:36:21 +0300

    r2r-triangle

---
+++ get-dac/r2r-triangle.py
@@ -0,0 +1,18 @@
+import signal_generator as sg
+from r2r_dac import R2R_DAC
+from time import time
+
+
+amplitude = 3
+signal_frequency = 10
+sampling_frequency = 1000
+
+
+if __name__ == "__main__":
+    try:
+        dac = R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3)
+        while True:
+            dac.set_voltage(amplitude * sg.get_triangle_amplitude(signal_frequency, time()))
+            sg.wait_for_sampling_period(sampling_frequency)
+    finally:
+        dac.deinit()
--- get-dac/signal_generator.py
+++ get-dac/signal_generator.py
@@ -8,5 +8,5 @@ def get_sin_wave_amplitude(freq: float, time: float):
 def wait_for_sampling_period(sampling_frequency: float):
     sleep(1.0 / sampling_frequency)
 
-# def get_triangle_amplitude(freq: float, time: float):
-#     return abs(2.0 * (time * freq - floor(time * freq)) - 1)
+def get_triangle_amplitude(freq: float, time: float):
+    return abs(2.0 * (time * freq - floor(time * freq)) - 1)
